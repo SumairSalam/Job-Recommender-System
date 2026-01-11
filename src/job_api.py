73c2259f53a8d2b0eab63269b8539bd8cb72a6fb
SUMAIR SALAM <sumairsalam9@gmail.com>
2026-01-11 16:13:03 +0100

    indeed jobs fetching functions through api

--- src/job_api.py
+++ src/job_api.py
@@ -1,43 +1,4 @@
-# import os
-# from apify_client import ApifyClient
-# from dotenv import load_dotenv
 
-# load_dotenv()
-
-# apify_client = ApifyClient(os.getenv("APIFY_API_TOKEN"))
-
-# def fetch_linkedin_jobs(search_query, location="Germany", rows=60):
-#     run_input = {
-#         "title": search_query,
-#         "location": location,
-#         "rows": rows,
-#         "proxy": {
-#             "useApifyProxy": True,
-#             "apifyProxyGroups": ["RESIDENTIAL"]
-#         }
-#     }
-#     run = apify_client.actor("hKByXkMQaC5Qt9UMN").call(run_input=run_input)
-#     jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
-#     return jobs
-
-# def fetch_indeed_jobs(search_query, location="Germany", rows=70):
-#     run_input = {
-#         "title": search_query,
-#         "location": location,
-#         "rows": rows,
-#         "proxy": {
-#             "useApifyProxy": True,
-#             "apifyProxyGroups": ["RESIDENTIAL"]
-#         }
-#     }
-#     run = apify_client.actor("apify/linkedin-jobs-scraper").call(
-#     run_input=run_input
-# )
-
-
-
-#     jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
-#     return jobs
 import os
 from apify_client import ApifyClient
 from dotenv import load_dotenv
@@ -66,7 +27,7 @@ def fetch_linkedin_jobs(search_query, location="Germany", rows=60):
 # -----------------------------
 # INDEED SCRAPER (WAITING FOR YOUR REAL ID)
 # -----------------------------
-def fetch_indeed_jobs(search_query, location="Germany", rows=60):
+def fetch_indeed_jobs(search_query, location="Germany", rows=50):
     run_input = {
         "query": search_query,
         "location": location,
